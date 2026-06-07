ea65e42a71e91103ca50367cc4fb3a7960e36cae
pator-pixel <pator@gmx.net>
2026-06-07 13:02:37 +0200

    Update hinzugefügt

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -63,5 +63,24 @@ def delete_post(post_id):
     }), 404
 
 
+@app.route('/api/posts/<int:post_id>', methods=['PUT'])
+def update_post(post_id):
+    data = request.get_json()
+
+    for post in POSTS:
+        if post["id"] == post_id:
+            if "title" in data:
+                post["title"] = data["title"]
+
+            if "content" in data:
+                post["content"] = data["content"]
+
+            return jsonify(post), 200
+
+    return jsonify({
+        "error": f"Post with id {post_id} not found."
+    }), 404
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
