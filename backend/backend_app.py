a4bd9f3b689f5fa6a89da6658ebae824128a60bc
pator-pixel <pator@gmx.net>
2026-06-07 13:00:04 +0200

    delete hinzugefügt

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -49,5 +49,19 @@ def add_post():
     return jsonify(new_post), 201
 
 
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    for post in POSTS:
+        if post["id"] == post_id:
+            POSTS.remove(post)
+            return jsonify({
+                "message": f"Post with id {post_id} has been deleted successfully."
+            }), 200
+
+    return jsonify({
+        "error": f"Post with id {post_id} not found."
+    }), 404
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
